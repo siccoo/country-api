3e4be3731c34b8ce105966e5e5f1e3de4eb85a7b
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-12-13 18:20:47 +0100

    material-ui, theme setup completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
-import Routes from './Routes';
+import React from "react";
+import Routes from "./Routes";
+
+import { createTheme } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
+
+// CREATE MATERIAL UI THEME
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#f2aa26',
+      dark: '#f09c01',
+    },
+    secondary: {
+      main: '#11cb5f',
+    },
+    text: {
+      primary: '#373585',
+      secondary: '#a4a643'
+    },
+    background: {
+      default: '#f7f8fc'
+    }
+  },
+  typography: {
+    fontFamily: 'Muli, sans-serif',
+    fontWeightBold: 600,
+    fontWeightMedium: 500,
+    fontWeightRegular: 400,
+    htmlFontSize: 8,
+  },
+})
 
 function App() {
   return (
-    <div className="App">
-      <Routes />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Routes />
+      </div>
+    </ThemeProvider>
   );
 }
 
