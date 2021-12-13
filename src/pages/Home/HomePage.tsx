1d623e95568efcf4aead5c4dee726ecd3e8192ba
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-12-13 21:16:47 +0100

    Navbar updates

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
 
 import "./homepage.scss";
 
 const HomePage = () => {
     return (
         <div className="home__container">
+            <Navbar />
             This is HomePage
         </div>
     )
