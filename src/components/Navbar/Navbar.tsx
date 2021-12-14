ea3950a81e4ce7a5d22c7b236a459cbfeeba3043
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-12-14 10:08:36 +0100

    updates on Navbar & material-ui icons package installed

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import MenuIcon from "@material-ui/icons/Menu";
+
 import "./navbar.scss";
 
 const Navbar = () => {
     return (
-        <div className="navbar__container"> 
-            This is the Navbar
-        </div>
+        <section className="navbar"> 
+            <div className="navbar__content container">
+                {/* LOGO AREA */}
+                <div className="navbar__content-left">
+                    <img src={process.env.PUBLIC_URL + "/images/country-api-logo-black.svg"} alt="country-api" />
+                </div>
+                {/* SEARCH BAR AREA */}
+                <div className="navbar__content-search">
+                    Search
+                </div>
+                {/* RIGHT ITEMS AREA */}
+                <div className="navbar__content-right">
+                    <ShoppingCartIcon/>
+                    <MenuIcon/>
+                </div>
+            </div>
+        </section>
     )
 }
 
