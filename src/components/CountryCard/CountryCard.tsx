f5b26534eac5ecd040aafe64a76f55f94e7ffa08
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-12-18 19:37:45 +0100

    updates on CountryCard.tsx and scss files

--- src/components/CountryCard/CountryCard.tsx
+++ src/components/CountryCard/CountryCard.tsx
@@ -10,8 +10,8 @@ const CountryCard = () => {
                 <img src="https://cdn.britannica.com/40/5340-004-B25ED5CF/Flag-Afghanistan.jpg" alt="country name" />
                 <h2 className="country__card__name">Afghanistan</h2>
                 <h2 className="country__card__region">Asia</h2>
+                <Button className="btn btn__primary">Add to cart</Button>
             </div>
-            <Button className="primary-cta">Add to cart</Button>
         </div>
         
     )
