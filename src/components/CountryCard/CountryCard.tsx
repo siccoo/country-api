774a4a43f70528bf92892c416ddbc1214bb7abe7
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-12-24 14:46:18 +0100

    declared CountryCard props interface

--- src/components/CountryCard/CountryCard.tsx
+++ src/components/CountryCard/CountryCard.tsx
@@ -2,13 +2,20 @@ import Button from "@material-ui/core/Button";
 
 import "./countrycard.scss";
 
-const CountryCard = () => {
+interface CountryCardProps {
+    flag: string
+    name: string
+    region: string
+}
+
+const CountryCard = (props: CountryCardProps) => {
+    const {flag, name, region} = props;
     return (
         <div className="country__card">
             <div className="country__card__wrapper">
-                <img src="https://cdn.britannica.com/40/5340-004-B25ED5CF/Flag-Afghanistan.jpg" alt="country name" />
-                <h2 className="country__card__name">Afghanistan</h2>
-                <h2 className="country__card__region">Asia</h2>
+                <img src={flag} alt={name} />
+                <h2 className="country__card__name">{name}</h2>
+                <h2 className="country__card__region">{region}</h2>
                 <Button className="btn btn__primary">Add to cart</Button>
             </div>
         </div>
--- src/components/CountryList/CountryList.tsx
+++ src/components/CountryList/CountryList.tsx
@@ -1,19 +1,20 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux"; 
+import { fetchAllCountries } from "../../redux/actions";
 import CountryCard from "../CountryCard/CountryCard";
 
 import "./countrylist.scss";
 
 const CountryList = () => {
+
+    
   return (
     <div className="country__list">
       {/* SORT COUNTRY */}
       <div className="country__list__sort"></div>
       {/* COUNTRY CARD LIST */}
       <div className="country__list__card">
-        <CountryCard />
-        <CountryCard />
-        <CountryCard />
-        <CountryCard />
+        <CountryCard flag={""} name={""} region={""} />
       </div>
       <div className="country__list__pagination"></div>
     </div>
