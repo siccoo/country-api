ba20315f7679c7674a3378f2aea4b870d9ceeaa4
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-12-24 13:09:44 +0100

    fetchAllCountries() in actions

--- src/redux/actions/countryActions.ts
+++ src/redux/actions/countryActions.ts
@@ -1,15 +1,18 @@
 import { FETCH_COUNTRIES_REQUEST, FETCH_COUNTRIES_SUCCESS, FETCH_COUNTRIES_FAILURE } from "../constants/countryConstants";
 import { CountryAction } from "../../types";
 
-// FETCH ALL COUNTRIES
-export function fetchAllCountries():CountryAction {
+import { Dispatch } from "redux";
+import axios from "axios";
+
+// FETCH ALL COUNTRIES REQUEST
+export function fetchAllCountriesRequest(): CountryAction {
     return {
         type: FETCH_COUNTRIES_REQUEST
     }
 }
 
 // FETCH ALL SUCCESS COUNTRIES
-export function fetchAllCountriesSuccess(countries: []):CountryAction {
+export function fetchAllCountriesSuccess(countries: []): CountryAction {
     return {
         type: FETCH_COUNTRIES_SUCCESS,
         payload: countries
@@ -17,9 +20,22 @@ export function fetchAllCountriesSuccess(countries: []):CountryAction {
 }
 
 // FETCH ALL FAILURE COUNTRIES
-export function fetchAllCountriesFailure(error: string):CountryAction {
+export function fetchAllCountriesFailure(error: string): CountryAction {
     return {
         type: FETCH_COUNTRIES_FAILURE,
         payload: error
+    }
+}
+
+// FETCH ALL COUNTRIES DATA
+export function fetchAllCountries() {
+    return (dispatch: Dispatch) => {
+        // AXIOS CALL
+        axios.get("https://restcountries.com/v3.1/all").then((res) => {
+            const countries = res.data;
+            dispatch(fetchAllCountriesSuccess(countries))
+        }).catch((err) => {
+            dispatch(fetchAllCountriesFailure(err))
+        })
     }
 }
