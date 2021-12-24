017a6b4f6c823404052fccb20c88541a3708b5fd
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-12-24 10:51:44 +0100

    updates on scss files

--- src/components/CountryList/CountryList.tsx
+++ src/components/CountryList/CountryList.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import CountryCard from '../CountryCard/CountryCard'
+import React from "react";
+import CountryCard from "../CountryCard/CountryCard";
+
+import "./countrylist.scss";
 
 const CountryList = () => {
-    return (
-        <div className="country__list">
-            <CountryCard />
-            <CountryCard />
-            <CountryCard />
-            <CountryCard />
-        </div>
-    )
-}
+  return (
+    <div className="country__list">
+      {/* SORT COUNTRY */}
+      <div className="country__list__sort"></div>
+      {/* COUNTRY CARD LIST */}
+      <div className="country__list__card">
+        <CountryCard />
+        <CountryCard />
+        <CountryCard />
+        <CountryCard />
+      </div>
+      <div className="country__list__pagination"></div>
+    </div>
+  );
+};
 
-export default CountryList
+export default CountryList;
